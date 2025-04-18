9c0743a4971ea459d26ece2fcf8a0cb9bae7c46e
519561910@qq.com <519561910@qq.com>
2025-04-18 18:02:02 +0800

    update modify-uboot-image.sh

--- scripts/modify-uboot-image.sh
+++ scripts/modify-uboot-image.sh
@@ -1,4 +1,4 @@
-# 复制rk3568-6.x-uboot.img文件到编译目录
+# 复制 rk3568-6.x-uboot.img 文件到编译的工作目录
 # cp -f $GITHUB_WORKSPACE/configfiles/rk3568-6.x-uboot.img rk3568-6.x-uboot.img
 # mnt/workdir/openwrt/bin/targets/rockchip/armv8/
 
